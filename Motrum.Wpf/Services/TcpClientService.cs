cdbb7ac4b8eeedeca93cc39e5dc91a4e592d1586
Nikolay <nikolayhudyakov18@gmail.com>
2026-04-16 15:21:10 +0400

    проверка на null производится после пинга так как в момент пинга клиент может стать null

--- Motrum.Wpf/Services/TcpClientService.cs
+++ Motrum.Wpf/Services/TcpClientService.cs
@@ -205,14 +205,14 @@ namespace Motrum.Wpf.Services
 
         private static bool GetConnectionStatus(TcpClient? client, string ipAddress)
         {
-            if (client?.Client == null)
+            if (client == null)
                 return false;
 
             using Ping ping = new();
             try
             {
-                return ping.Send(ipAddress, PingTimeout).Status == IPStatus.Success &&
-                    !(client.Client.Poll(0, SelectMode.SelectRead) && client.Client.Available == 0) &&
+                return ping.Send(ipAddress, PingTimeout).Status == IPStatus.Success && 
+                    client.Client != null && !(client.Client.Poll(0, SelectMode.SelectRead) && client.Client.Available == 0) &&
                     client.Connected;
             }
             catch
